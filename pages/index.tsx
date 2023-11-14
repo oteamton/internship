53410172d4db199ae74d05ab215e9aef334f3ce3
Kitcht <oteamton@gmail.com>
2023-11-14 16:36:36 +0700

    Changed header

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Welcome to My Next.js App</h1>
+      <h1 className={styles.heading}>SWU Internship</h1>
       <Link href="/student"><button className={styles.button}> Student</button></Link>
       <Link href="/company"><button className={styles.button}> Company</button></Link>
     </div>
