9eaa91e0a46a644086aec100695ad1b4d464be53
Kitcht <oteamton@gmail.com>
2023-11-17 15:26:33 +0700

    Changed to static styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,47 +18,6 @@ const Home: React.FC = () => {
     resolver: yupResolver(validationSchema) as any,
   });
 
-  const [visibleSection, setVisibleSection] = useState<number>(0);
-  const [scrollPosition, setScrollPosition] = useState<number>(0);
-
-  const handleScroll = () => {
-    setScrollPosition(window.scrollY);
-  };
-
-  useEffect(() => {
-    const handleScrollThrottled = throttle(handleScroll, 200);
-
-    window.addEventListener('scroll', handleScrollThrottled);
-
-    return () => {
-      window.removeEventListener('scroll', handleScrollThrottled);
-    };
-  }, []);
-
-  useEffect(() => {
-    const sectionOffsets = Array.from(
-      document.querySelectorAll('section')
-    ).map((section) => section.offsetTop);
-
-    const findVisibleSection = () => {
-      const currentPosition = scrollPosition + window.innerHeight / 2;
-
-      for (let i = 0; i < sectionOffsets.length; i++) {
-        if (currentPosition < sectionOffsets[i]) {
-          setVisibleSection(i - 1);
-          break;
-        }
-      }
-
-      // If the scroll position is beyond the last section, set it to the last section
-      if (currentPosition >= sectionOffsets[sectionOffsets.length - 1]) {
-        setVisibleSection(sectionOffsets.length - 1);
-      }
-    };
-
-    findVisibleSection();
-  }, [scrollPosition]);
-
   const onSubmit: SubmitHandler<LoginValues> = (data) => {
     console.log('Form data submitted:', data);
   }
@@ -71,7 +30,7 @@ const Home: React.FC = () => {
   return (
     <div className={styles.container}>
 
-      <section className={`${styles.wrapper_register} ${visibleSection === 0 ? styles.fadeIn : styles.fadeOut}`}>
+      <section className={styles.wrapper_register}>
         {/* <BackgroundImage /> */}
         <div className={styles.content_register}>
           <h1 className={styles.heading}>SWU <span>Internship</span></h1>
@@ -82,7 +41,7 @@ const Home: React.FC = () => {
         </div>
       </section>
 
-      <section className={`${styles.wrapper_partners} ${visibleSection === 0 ? styles.fadeIn : styles.fadeOut}`}>
+      <section className={styles.wrapper_partners}>
         <h1>Partners</h1>
         <div className={styles.content_partners}>
           <img src="" alt="" />
@@ -94,7 +53,7 @@ const Home: React.FC = () => {
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus tempora vitae fugit voluptatem ducimus, consequuntur vel eos magnam quo optio, repudiandae quisquam tenetur voluptates quis rem reprehenderit non ea. Neque.</p>
       </section>
 
-      <section className={`${styles.wrapper_partners} ${visibleSection === 0 ? styles.fadeIn : styles.fadeOut}`}>
+      <section className={styles.wrapper_login}>
         <div className={styles.content_login}>
           <h1>Login to <span id={styles.heading}>SWU <span>Internship</span></span></h1>
 
