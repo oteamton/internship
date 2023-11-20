33ebf9d133669d1901437638112fd08859246c2a
Kitcht <oteamton@gmail.com>
2023-11-20 16:45:39 +0700

    Added serverless structure and working with Mysql

---
+++ pages/api/postData.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connToDatabase, disconnFromDB } from "../../utils/db";
+import { InsertData } from "../../interfaces";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const conn = await connToDatabase();
+
+        try {
+            const { table, data } = req.body as InsertData;
+            if (!table || !data) {
+                return res.status(400).json({ success: false, message: 'Invalid request body' });
+            }
+
+            const placeholders = Object.keys(data).map(() => '?').join(',');
+            const columns = Object.keys(data).join(',');
+
+            const query = `INSERT INTO ${table} (${columns}) VALUES (${placeholders})`;
+            console.log('Executing query:', query);
+            const [results] = await conn.execute(query, Object.values(data));
+
+            console.log('Data inserted successfully:', results);
+
+            res.status(200).json({ success: true, message: 'Data inserted successfully' });
+        } catch (error) {
+            console.error('Error inserting data:', error);
+            // console.error('Failed query:', query);
+            res.status(500).json({ success: false, message: 'Internal Server Error' });
+        } finally {
+            await disconnFromDB();
+        }
+    } else {
+        res.status(405).json({ success: false, message: 'Method Not Allowed' });
+    }
+}
