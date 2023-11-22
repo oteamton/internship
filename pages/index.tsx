53a1410e3ef30f4adb2d3eb577dd03d0d262723e
Kitcht <oteamton@gmail.com>
2023-11-22 14:27:14 +0700

    Structured and Styled

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { LoginValues } from '../interfaces';
 import { useForm, SubmitHandler, SubmitErrorHandler } from 'react-hook-form';
@@ -31,18 +31,20 @@ const Home: React.FC = () => {
       <header>
         <img src="https://swu.ac.th/images/swu_logo_v3.png" alt="" />
         <nav>
-          <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-          <Link href="/users">Contact</Link> {' '}
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/users">Contact</Link>
           {/* <a href="/api/users">Users API</a> */}
         </nav>
       </header>
-
       <section className={styles.wrapper_register}>
         <h1 className={styles.heading}>SWU <span>Internship</span></h1>
         <div className={styles.content_register}>
-          <img src='https://p7.hiclipart.com/preview/643/201/294/stick-figure-student-presentation-study-skills-animation-think.jpg' />
-          <img src='https://p7.hiclipart.com/preview/643/201/294/stick-figure-student-presentation-study-skills-animation-think.jpg' />
+          <p className={styles.p_stu}>Welcome Student</p>
+          <img id={styles.stu} src="https://media-public.canva.com/su8kk/MAEfd5su8kk/1/tl.png" alt="" />
           <button className={styles.btn_stu}><Link href="/student">Student</Link></button>
+          <p className={styles.p_com}>Welcome Company</p>
+          <img id={styles.com} src="https://media-public.canva.com/ZfTZA/MAFcrqZfTZA/1/tl.png" alt="" />
           <button className={styles.btn_com}><Link href="/company">Company</Link></button>
         </div>
       </section>
@@ -52,7 +54,16 @@ const Home: React.FC = () => {
       </section>
 
       <footer>
-        <span>I'm here to stay (Footer)</span>
+        <div className={styles.link}>
+          <Link href="https://ipass.swu.ac.th/">I-Pass</Link> | <Link href="/about">GAFE Mail</Link> |{' '}
+          <Link href="https://supreme.swu.ac.th/portal/index.php">SUPREME</Link> {' '}| <Link href="http://search.swu.ac.th/primo-explore/search?vid=SWU">Contact us</Link> |{' '} <Link href="https://account.swu.ac.th/directory">Directory</Link> |{' '}<Link href="https://www.swu.ac.th/contact.php">Contact us</Link> |{' '}<Link href="https://www.swu.ac.th/location.php">SWU Map</Link> |{' '} <Link href="https://swu.ac.th/quicklink.html">Quick Link</Link>
+        </div>
+        <div className={styles.social}>
+          <img src="https://swu.ac.th/images/facebook.png" alt="" />
+          <img src="https://swu.ac.th/images/twitter.png" alt="" />
+          <img src="https://swu.ac.th/images/youtube.png" alt="" />
+          <img src="https://swu.ac.th/images/instagram.png" alt="" />
+        </div>
       </footer>
 
     </div >
